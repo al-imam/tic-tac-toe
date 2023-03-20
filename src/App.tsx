6460df39969ddf1709bcc4801d80d41bc7c7c01f
al-imam <alimam01828@gmail.com>
2023-03-20 16:07:26 +0600

    create 10 din

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div>Hello word</div>;
+  return (
+    <main>
+      <div className="h-10 bg-red-700 w-10 inline-block"></div>
+      <div className="h-10 bg-blue-700 w-10 inline-block"></div>
+      <div className="h-10 bg-red-700 w-10 inline-block"></div>
+      <div className="h-10 bg-teal-700 w-10 inline-block"></div>
+      <div className="h-10 bg-red-700 w-10 inline-block"></div>
+      <div className="h-10 bg-purple-700 w-10 inline-block"></div>
+      <div className="h-10 bg-red-700 w-10 inline-block"></div>
+      <div className="h-10 bg-indigo-700 w-10 inline-block"></div>
+      <div className="h-10 bg-red-700 w-10 inline-block"></div>
+      <div className="h-10 bg-yellow-700 w-10 inline-block"></div>
+    </main>
+  );
 }
 
 export default App;
