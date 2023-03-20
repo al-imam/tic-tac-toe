fa655119bfb8e1a0eb3f0db9d0f5f3f4331658fc
al-imam <alimam01828@gmail.com>
2023-03-20 19:15:16 +0600

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 function App() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <div className="grid grid-cols-3 grid-rows-3">
-        <div className="h-10 bg-blue-700 w-10 inline-block"></div>
-        <div className="h-10 bg-red-700 w-10 inline-block"></div>
-        <div className="h-10 bg-teal-700 w-10 inline-block"></div>
-        <div className="h-10 bg-red-700 w-10 inline-block"></div>
-        <div className="h-10 bg-purple-700 w-10 inline-block"></div>
-        <div className="h-10 bg-red-700 w-10 inline-block"></div>
-        <div className="h-10 bg-indigo-700 w-10 inline-block"></div>
-        <div className="h-10 bg-red-700 w-10 inline-block"></div>
-        <div className="h-10 bg-yellow-700 w-10 inline-block"></div>
+      <div className="grid grid-cols-3 grid-rows-3 ">
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
+        <div className="w-40 h-40"></div>
       </div>
     </main>
   );
