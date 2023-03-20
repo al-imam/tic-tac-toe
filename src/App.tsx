2564239a56b079eca8af66df8aaf5ddae28a0546
al-imam <alimam01828@gmail.com>
2023-03-20 16:11:11 +0600

    layout element in 3 by 3 grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 function App() {
   return (
     <main>
-      <div className="h-10 bg-red-700 w-10 inline-block"></div>
-      <div className="h-10 bg-blue-700 w-10 inline-block"></div>
-      <div className="h-10 bg-red-700 w-10 inline-block"></div>
-      <div className="h-10 bg-teal-700 w-10 inline-block"></div>
-      <div className="h-10 bg-red-700 w-10 inline-block"></div>
-      <div className="h-10 bg-purple-700 w-10 inline-block"></div>
-      <div className="h-10 bg-red-700 w-10 inline-block"></div>
-      <div className="h-10 bg-indigo-700 w-10 inline-block"></div>
-      <div className="h-10 bg-red-700 w-10 inline-block"></div>
-      <div className="h-10 bg-yellow-700 w-10 inline-block"></div>
+      <div className="grid grid-cols-3 grid-rows-3">
+        <div className="h-10 bg-red-700 w-10 inline-block"></div>
+        <div className="h-10 bg-blue-700 w-10 inline-block"></div>
+        <div className="h-10 bg-red-700 w-10 inline-block"></div>
+        <div className="h-10 bg-teal-700 w-10 inline-block"></div>
+        <div className="h-10 bg-red-700 w-10 inline-block"></div>
+        <div className="h-10 bg-purple-700 w-10 inline-block"></div>
+        <div className="h-10 bg-red-700 w-10 inline-block"></div>
+        <div className="h-10 bg-indigo-700 w-10 inline-block"></div>
+        <div className="h-10 bg-red-700 w-10 inline-block"></div>
+        <div className="h-10 bg-yellow-700 w-10 inline-block"></div>
+      </div>
     </main>
   );
 }
