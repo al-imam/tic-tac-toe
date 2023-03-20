54bd4105b089b86ccbf3f40e0118240b535e9f36
al-imam <alimam01828@gmail.com>
2023-03-20 19:34:15 +0600

    add class to style childs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <div className="grid grid-cols-3 grid-rows-3 ">
+      <div className="grid grid-cols-3 grid-rows-3 grid-layout">
         <div className="w-40 h-40"></div>
         <div className="w-40 h-40"></div>
         <div className="w-40 h-40"></div>
