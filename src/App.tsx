a4b11fa6fcbba194f310841555fe341d39285a06
al-imam <alimam01828@gmail.com>
2023-03-20 20:23:02 +0600

    add cell class to child

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 function App() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <div className="grid grid-cols-3 grid-rows-3 grid-layout">
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
-        <div className="w-40 h-40"></div>
+      <div className="grid grid-cols-3 grid-rows-3 grid-layout ">
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
+        <div className="cell w-40 h-40"></div>
       </div>
     </main>
   );
