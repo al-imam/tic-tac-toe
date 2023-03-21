d4eeae7054d84ccd7cb187b24e00336e5fdd36c5
al-imam <alimam01828@gmail.com>
2023-03-21 20:53:08 +0600

    add cross svg

--- src/svg/Cross.tsx
+++ src/svg/Cross.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 
-export function Cross(props: SVGProps<SVGSVGElement>) {
+export default function Cross(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
