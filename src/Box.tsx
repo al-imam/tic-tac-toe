088eef3595645e9a051efe0afd74d85409817f91
al-imam <alimam01828@gmail.com>
2023-03-22 01:21:36 +0600

    change class names

--- src/Box.tsx
+++ src/Box.tsx
@@ -8,7 +8,7 @@ export default function Box({
   return (
     <div
       onClick={onTap}
-      className={`cell w-40 h-40 flex justify-center items-center ${classes}`}
+      className={`box w-40 h-40 flex justify-center items-center ${classes}`}
     ></div>
   );
 }
