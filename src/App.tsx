ae3d3a44ccc3631a229f2d51a41ede6055758da6
al-imam <alimam01828@gmail.com>
2023-03-21 22:02:22 +0600

    add winning combination

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,17 @@ const init = {
   win: null,
 };
 
+const winningCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function reducer(
   { grid, current, win }: typeof init,
   { index, node }: { index: number; node: string }
